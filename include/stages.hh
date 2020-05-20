54aa73df1991bd0484dcb984402a4dc071edcaa7
Shervin Nourbakhsh <nourbakhsh@ill.fr>
2020-05-20 11:54:51 +0200

    update

--- include/stages.hh
+++ include/stages.hh
@@ -1,28 +1,26 @@
 #ifndef STAGES_HEADER
 #define STAGES_HEADER
-#include<string>
-/** \ingroup clientAPI 
- * global definitions 
+#include <map>
+#include <string>
+/** \ingroup clientAPI
+ * global definitions */
+namespace panosc_sim_server
+{
+
+/** stages implemented as integers to allow for loops
  */
-namespace panosc_sim_server{
+typedef size_t stage_t;
+/**
+ * @{
+ */
+static const stage_t sFULL     = 2; ///< FULL detector simulation stage
+static const stage_t sSAMPLE   = 1; ///< simulation from source, excluding the sample and following parts
+static const stage_t sDETECTOR = 0; ///< simulation including the sample, not the detector
+                                    /// @}
 
-
-  /** stages implemented as integers to allow for loops
-   */
-  typedef size_t stage_t;
-  /** 
-   * @{
-   */ 
-  static const stage_t sFULL     = 2; ///< FULL detector simulation stage 
-  static const stage_t sSAMPLE   = 1; ///< simulation from source, excluding the sample and following parts 
-  static const stage_t sDETECTOR = 0; ///< simulation including the sample, not the detector 
-  /// @}
-
-  ///\brief here's the string names of the stages, useful for output directories, etc.
-  //static const std::vector<std::string> stages = {"sDETECTOR", "sSAMPLE", "sFULL"};
-  static const std::map<stage_t,std::string> stages;
-  stages[sDETECTOR]="sDETECTOR";
-  stages[sSAMPLE]="sSAMPLE";
-  stages[sFULL]="sFULL";
-}
+///\brief here's the string names of the stages, useful for output directories, etc.
+// static const std::vector<std::string> stages = {"sDETECTOR", "sSAMPLE", "sFULL"};
+static const std::map<stage_t, std::string> stages = {
+    {sDETECTOR, "sDETECTOR"}, {sSAMPLE, "sSAMPLE"}, {sFULL, "sFULL"}};
+} // namespace panosc_sim_server
 #endif
